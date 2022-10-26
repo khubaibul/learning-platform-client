e11ada27f3a4b24593ed60a1fd7913c4a7907e4f
Khubaibul Islam <khubaibulislam2001@gmail.com>
2022-10-26 19:54:49 +0600

    Courses pages some design updated and Readme.md file also updated

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 - This web app, users can interact and can get information about this web app.
 - Users can see course details and course information.
 - After visiting the course detail page, if the user is willing to get premium access, he can.
-- To get premium access user must need to log in or signup first. It’s the authentication system of this web app.
+- To get premium access user must need to log in or sign up first. It’s the authentication system of this web app.
 - The checkout page is private. Only authorized and authenticated users can visit this page.
 - This web app also has a user profile page where the user can see his/her information alongside his photo.
 - Users can easily be logged out from the user profile page by clicking Log Out.
@@ -15,11 +15,12 @@
 
 ### The Technology Used In CSE From Home Web Applications
 
-1. React
-2. Firebase
-3. Express
-4. Node js
-5. Tailwind (As CSS Framework)
-6. DaisyUI (As Tailwind Library
-7. FontAwesome + FlatIcons (As Icon Package)
-8. React-To-PDF (As PDF Downloader)
+- React
+- Firebase
+- Express
+- Node js
+- Tailwind (As CSS Framework)
+- DaisyUI (As Tailwind Library
+- FontAwesome + FlatIcons (As Icon Package)
+- React-To-PDF (As PDF Downloader)
+- React Router Dom (For SPA)
--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -5,7 +5,6 @@ import "./Courses.css";
 
 const Courses = () => {
     const allCourses = useLoaderData();
-    console.log(allCourses);
     return (
         <div className='lg:px-20 mt-20'>
             <h2 className='text-center text-3xl font-bold'>Our All Course</h2>
@@ -13,23 +12,28 @@ const Courses = () => {
                 {
                     allCourses.map(course => {
                         return <div key={course.courseId} className='flex'>
-                            <Link to={`/course/${course.courseId}`} className='mb-2 w-full btn btn-outline btn-accent rounded-none'>{course.courseTitle}</Link>
+                            <Link to={`/course/${course.courseId}`} className='mb-2 w-full btn btn-outline btn-accent rounded-none'>
+                                <img className='w-5 mr-2' src={course.icon} alt="" />
+                                {course.courseTitle}
+                            </Link>
                         </div>
                     })
                 }
             </div>
-            {/* courses-page */}
             <div className='grid grid-cols-5'>
                 <div className='px-4 lg:block hidden'>
                     {
                         allCourses.map(course => {
                             return <div key={course.courseId} className='flex'>
-                                <Link to={`/course/${course.courseId}`} className='mb-2 w-full btn btn-outline btn-accent rounded-none'>{course.courseTitle}</Link>
+                                <Link to={`/course/${course.courseId}`} className='mb-2 w-full btn btn-outline btn-accent rounded-none'>
+                                    <img className='w-5 mr-2' src={course.icon} alt="" />
+                                    {course.courseTitle}
+                                </Link>
                             </div>
                         })
                     }
                 </div>
-                <div className='grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 col-span-4'>
+                <div className='grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 lg:col-span-4 col-span-5'>
                     {
                         allCourses.map(singleCourse => <SingleCourse key={singleCourse.courseId} singleCourse={singleCourse}></SingleCourse>)
                     }
