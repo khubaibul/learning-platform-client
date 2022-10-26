ca026d299b54d2f3599462fcfb554ee309efa80e
Khubaibul Islam <khubaibulislam2001@gmail.com>
2022-10-26 18:36:09 +0600

    PDF download system implement completed

--- src/Pages/CourseDetail/CourseDetail.js
+++ src/Pages/CourseDetail/CourseDetail.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFilePdf, faTools } from '@fortawesome/free-solid-svg-icons';
 import { Link, useLoaderData } from 'react-router-dom';
-// import { PDFViewer } from '@react-pdf/renderer';
-// import PDF from '../PDF/PDF';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import PDF from '../PDF/PDF';
 
 const CourseDetail = () => {
     const courseDetail = useLoaderData();
@@ -15,21 +15,20 @@ const CourseDetail = () => {
             <div className="card-body rounded-none">
                 <div className='flex justify-between mb-5'>
                     <h2 className="card-title text-4xl">{courseTitle}</h2>
-                    <button>
-                        <FontAwesomeIcon className='text-3xl' icon={faFilePdf}></FontAwesomeIcon>
-                        {/* <PDFViewer>
-                            <PDF courseDetail={courseDetail}></PDF>
-                        </PDFViewer> */}
-                    </button>
+                    <PDFDownloadLink document={<PDF courseDetail={courseDetail} />} filename="FORM">
+                        {({ loading }) => (loading ? <button>Loading Document...</button> : <button>
+                            <FontAwesomeIcon className='text-3xl' icon={faFilePdf}></FontAwesomeIcon>
+                        </button>)}
+                    </PDFDownloadLink>
                 </div>
                 <p className='text-justify'><span className='font-bold text-lg'>Course Description: </span> {course_description}</p>
                 <p><span className='font-bold text-lg'>Common Uses: </span> {common_uses}</p>
                 <div>
-                    <h4 className='text-xl font-bold my-2'>Will Be Coverd</h4>
+                    <h4 className='text-xl font-bold my-2'>Will Be Covered</h4>
                     <div className='grid grid-cols-2'>
                         {
                             course_module_topics.map((topic, idx) => {
-                                return <div>
+                                return <div key={idx}>
                                     <li className='list-none'><FontAwesomeIcon icon={faTools}></FontAwesomeIcon> {topic}</li>
                                 </div>
                             })
--- src/Pages/PDF/PDF.js
+++ src/Pages/PDF/PDF.js
@@ -1,41 +1,72 @@
-// import React from 'react';
-// import { Document, Page, Text, View, StyleSheet, Image } from "@react-pdf/renderer";
+import React from 'react';
+import { Document, Page, Text, View, StyleSheet, Image } from "@react-pdf/renderer";
 
-// const styles = StyleSheet.create({
-//     page: {
-//         flexDirection: 'column',
-//         backgroundColor: '#E4E4E4',
-//     },
-//     section: {
-//         fontSize: 30,
-//         textAlign: "center",
-//         flexGrow: 1
-//     },
-//     image: {
-//         // marginVertical: 15,
-//         // marginHorizontal: 100,
-//     }
-// });
+const styles = StyleSheet.create({
+    page: {
+        flexDirection: 'column',
+        backgroundColor: '#E4E4E4',
+    },
+    section: {
+        fontSize: 40,
+        textAlign: "center",
+        marginBottom: 30,
+        color: "#c44569",
+        fontWeight: "extrabold"
+    },
+    image: {
+        borderRadius: 10,
+    },
+    description: {
+        marginTop: 20,
+        color: "#303952",
+        fontWeight: "bold",
+        marginLeft: 20,
+        marginRight: 20
+    },
+    common_uses: {
+        marginTop: 20,
+        color: "#303952"
+    },
+    info: {
 
 
-// const PDF = ({ courseDetail }) => {
-//     console.log(courseDetail);
-//     return (
-//         <Document>
-//             <Page size="A4" style={styles.page}>
-//                 <View style={styles.section}>
-//                     <Text>{courseDetail.courseTitle}</Text>
-//                 </View>
-//                 <Image
-//                     style={styles.image}
-//                     src={courseDetail.courseImage}
-//                 />
-//                 <Text>
-//                     {courseDetail.course_description}
-//                 </Text>
-//             </Page>
-//         </Document>
-//     );
-// };
+    }
+});
 
-// export default PDF;
+
+const PDF = ({ courseDetail }) => {
+    const { common_uses, courseImage, courseTitle, courseId, course_description, course_duration, course_module, course_price, course_module_topics } = courseDetail;
+    return (
+        <Document>
+            <Page size="A4" style={styles.page}>
+                <View style={styles.section}>
+                    <Text>{courseTitle}</Text>
+                </View>
+                <Image
+                    style={styles.image}
+                    src={courseImage}
+                />
+                <Text style={styles.description}>
+                    Description: {course_description}
+                </Text>
+                <Text  style={styles.description}>
+                    Common Uses: {courseDetail.common_uses}
+                </Text>
+                <Text  style={styles.description}>
+                    Course ID:{courseId}
+                </Text>
+                <Text style={styles.description}>
+                    Course Duration: {course_duration}
+                </Text>
+                <Text style={styles.description}>
+                    Total Module: {course_module}
+                </Text>
+                <Text style={styles.description}>
+                    Price: BDT{course_price}
+                </Text>
+            </Page>
+        </Document>
+    );
+};
+
+export default PDF;
