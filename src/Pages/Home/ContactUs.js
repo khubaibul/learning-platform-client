2bd6492bd9b38aa70b958e938da63a03828329bd
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-09-26 23:48:00 +0600

    Google Map Added

--- src/Pages/Home/ContactUs.js
+++ src/Pages/Home/ContactUs.js
@@ -1,8 +1,19 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../Context/AuthProvider/AuthProvider";
+import GoogleMapReact from "google-map-react";
 
 const ContactUs = () => {
   const { theme } = useContext(AuthContext);
+
+  const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+  const defaultProps = {
+    center: {
+      lat: 22.335,
+      lng: 91.8325,
+    },
+    zoom: 5,
+  };
 
   return (
     <div className="container my-24 mx-auto md:px-6 lg:w-[90%]">
@@ -88,8 +99,8 @@ const ContactUs = () => {
               </button>
             </form>
           </div>
-          <div className="w-full shrink-0 grow-0 basis-auto lg:w-7/12">
-            <div className="flex flex-wrap">
+          <div className="w-full flex lg:w-7/12">
+            <div className="">
               <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
                 <div className="flex items-start">
                   <div className="shrink-0">
@@ -124,70 +135,6 @@ const ContactUs = () => {
                 </div>
               </div>
               <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
-                <div className="flex items-start">
-                  <div className="shrink-0">
-                    <div className="inline-block rounded-md bg-primary-100 p-4 text-primary">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="2"
-                        stroke="currentColor"
-                        className="h-6 w-6"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                  <div className="ml-6 grow">
-                    <p className="mb-2 font-bold dark:text-white">
-                      Student Support
-                    </p>
-                    <p className="text-neutral-500 dark:text-neutral-200">
-                      csefromhome@studentsupport.com
-                    </p>
-                    <p className="text-neutral-500 dark:text-neutral-200">
-                      +1 234-567-89
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
-                <div className="align-start flex">
-                  <div className="shrink-0">
-                    <div className="inline-block rounded-md bg-primary-100 p-4 text-primary">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="2"
-                        stroke="currentColor"
-                        className="h-6 w-6"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                  <div className="ml-6 grow">
-                    <p className="mb-2 font-bold dark:text-white">Press</p>
-                    <p className="text-neutral-500 dark:text-neutral-200">
-                      press@example.com
-                    </p>
-                    <p className="text-neutral-500 dark:text-neutral-200">
-                      +880 1832 30 21 70
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:px-6">
                 <div className="align-start flex">
                   <div className="shrink-0">
                     <div className="inline-block rounded-md bg-primary-100 p-4 text-primary">
@@ -208,7 +155,9 @@ const ContactUs = () => {
                     </div>
                   </div>
                   <div className="ml-6 grow">
-                    <p className="mb-2 font-bold dark:text-white">Assignment Evaluate</p>
+                    <p className="mb-2 font-bold dark:text-white">
+                      Assignment Evaluate
+                    </p>
                     <p className="text-neutral-500 dark:text-neutral-200">
                       csefromhome@assignment.com
                     </p>
@@ -218,6 +167,18 @@ const ContactUs = () => {
                   </div>
                 </div>
               </div>
+            </div>
+
+            <div className="w-full mx-auto rounded" style={{ height: "60vh" }}>
+              <GoogleMapReact
+                bootstrapURLKeys={{
+                  key: "AIzaSyBtxjZlOvgBUYikrge54BE-RfbsJ--BbJ8",
+                }}
+                defaultCenter={defaultProps.center}
+                defaultZoom={defaultProps.zoom}
+              >
+                <AnyReactComponent lat={22.335} lng={91.8325} text="Shakib" />
+              </GoogleMapReact>
             </div>
           </div>
         </div>
