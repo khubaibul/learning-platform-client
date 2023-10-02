7ecf933abacb7fb4ce78033aeacf16c106797cd1
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-10-02 23:49:59 +0600

    Login Pages Some Color Improved

--- src/Pages/User/SignUp/SignUp.js
+++ src/Pages/User/SignUp/SignUp.js
@@ -127,12 +127,12 @@ const SignUp = () => {
           theme ? "bg-slate-700" : "bg-white"
         } lg:w-5/12 md:6/12 w-10/12 shadow-3xl`}
       >
-        <div className="bg-gray-800 absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full p-4 md:p-8">
-          <svg width="32" height="32" viewBox="0 0 24 24" fill="#FFF">
+        <div className="bg-yellow-400 border-4 border-red-500 absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full md:p-8">
+          <svg width="50" height="50" viewBox="0 0 24 24" fill="#EF4444">
             <path d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z" />
           </svg>
         </div>
-        <form onSubmit={handleSignUp} className="p-12 md:p-24">
+        <form onSubmit={handleSignUp} className="bg-red-500 md:p-24">
           <div className="flex items-center text-lg mb-6 md:mb-8">
             <svg className="absolute ml-3" width="24" viewBox="0 0 24 24">
               <path d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z" />
@@ -199,7 +199,7 @@ const SignUp = () => {
                 <input
                   onClick={handleTermsAndConditions}
                   type="checkbox"
-                  className="checkbox"
+                  className="checkbox border-yellow-400"
                 />
                 <span className={`label-text ${theme && "text-slate-300"}`}>
                   Accept Terms & Conditions
@@ -218,7 +218,7 @@ const SignUp = () => {
           </div>
           <button
             disabled={!accepted}
-            className="bg-gradient-to-b from-gray-700 to-gray-900 font-medium font-montserrat p-2 md:p-4 text-white uppercase w-full"
+            className="bg-gradient-to-b from-red-500 to-red-600 border-2 border-yellow-400 hover:from-red-600 hover:to-red-500 font-medium font-montserrat p-2 md:p-4 text-white uppercase w-full"
           >
             Sign Up
           </button>
