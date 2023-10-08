bcf24c9d081468a0b37c8be170e1d1fd554a5367
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-10-08 18:39:36 +0600

    Message Implemented

--- src/Pages/Home/ContactUs.js
+++ src/Pages/Home/ContactUs.js
@@ -43,7 +43,9 @@ const ContactUs = () => {
               <div className="relative mb-6" data-te-input-wrapper-init>
                 <input
                   type="text"
-                  className={`peer block min-h-[auto] w-full rounded border-0 py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 ${theme ? "bg-gray-600" : "bg-base-100"}`}
+                  className={`peer block min-h-[auto] w-full rounded border-0 py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 ${
+                    theme ? "bg-gray-600" : "bg-base-100"
+                  }`}
                   id="exampleInput90"
                   placeholder="Name"
                 />
@@ -57,7 +59,9 @@ const ContactUs = () => {
               <div className="relative mb-6" data-te-input-wrapper-init>
                 <input
                   type="email"
-                  className={`peer block min-h-[auto] w-full rounded border-0 py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-gray-800 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-gray-800 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 ${theme ? "bg-gray-600" : "bg-base-100"}`}
+                  className={`peer block min-h-[auto] w-full rounded border-0 py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-gray-800 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-gray-800 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 ${
+                    theme ? "bg-gray-600" : "bg-base-100"
+                  }`}
                   id="exampleInput91"
                   placeholder="Email address"
                 />
@@ -70,7 +74,9 @@ const ContactUs = () => {
               </div>
               <div className="relative mb-6" data-te-input-wrapper-init>
                 <textarea
-                  className="peer block min-h-[auto] w-full rounded border-0 bg-base-100 py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  className={`peer block min-h-[auto] w-full rounded border-0 py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 ${
+                    theme ? "bg-gray-600" : "bg-base-100"
+                  }`}
                   id="exampleFormControlTextarea1"
                   rows="6"
                   placeholder="Your message"
