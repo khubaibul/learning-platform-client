c13b037d2b50cf0c7554ae0ece409d8cae96a48b
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-10-08 18:38:25 +0600

    Dark Mode BG Has Changed

--- src/Pages/Home/ContactUs.js
+++ src/Pages/Home/ContactUs.js
@@ -43,7 +43,7 @@ const ContactUs = () => {
               <div className="relative mb-6" data-te-input-wrapper-init>
                 <input
                   type="text"
-                  className="peer block min-h-[auto] w-full rounded border-0 bg-base-100 py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  className={`peer block min-h-[auto] w-full rounded border-0 py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 ${theme ? "bg-gray-600" : "bg-base-100"}`}
                   id="exampleInput90"
                   placeholder="Name"
                 />
