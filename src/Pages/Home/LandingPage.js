8b3c0af18c06bbd0c7e57438042466ff53a35297
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-10-25 22:40:38 +0600

    Students Section Updated

--- src/Pages/Home/LandingPage.js
+++ src/Pages/Home/LandingPage.js
@@ -185,7 +185,7 @@ const LandingPage = () => {
                   <div className="grid lg:grid-cols-2 grid-cols-1 gap-x-20 pl-16 my-5">
                     <div>
                       <img
-                        className="lg:w-[450px] lg:h-[350px]"
+                        className="lg:w-[450px] lg:h-[350px] md:w-[350px] md:"
                         src={students}
                         alt="Students"
                       />
