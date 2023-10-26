de711f1c62e0e5685172a196c65f1d93462db7c2
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-10-26 22:46:28 +0600

    MyClass Page Created

--- src/Pages/MyClasses/MyClass.js
+++ src/Pages/MyClasses/MyClass.js
@@ -1,22 +1,11 @@
-import React from "react";
+import React from 'react';
 
-const MyClass = ({ course }) => {
-  return (
-    <div>
-      <div>
+const MyClass = () => {
+    return (
         <div>
-          <video src="https://youtu.be/F9UC9DY-vIU?si=3AT2KwWHS4wcTn2V"></video>
+            
         </div>
-        <ul>
-          <li>Class 1</li>
-          <li>Class 2</li>
-          <li>Class 3</li>
-          <li>Class 1</li>
-          <li>Class 4</li>
-        </ul>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default MyClass;
