9c4209639abd2dbf4c3ffdec3326110fe89ba09a
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-10-26 22:47:12 +0600

    All Classes Are Added By Listing

--- src/Pages/MyClasses/MyClass.js
+++ src/Pages/MyClasses/MyClass.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const MyClass = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const MyClass = ({ course }) => {
+  return (
+    <div>
+      <div>
+        div
+        <ul>
+          <li>Class 1</li>
+          <li>Class 2</li>
+          <li>Class 3</li>
+          <li>Class 1</li>
+          <li>Class 4</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default MyClass;
