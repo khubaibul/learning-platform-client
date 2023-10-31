021b19e7d4726d1ceb60ee265a8c941c878fd888
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-10-31 23:48:34 +0600

    Class videos added with all class lists

--- src/Pages/MyClasses/MyClass.js
+++ src/Pages/MyClasses/MyClass.js
@@ -2,12 +2,18 @@ import React from "react";
 
 const MyClass = ({ course }) => {
   return (
-    <div>
-      <div>
+    <div className="w-[80%] mx-auto">
+      <h1 className="text-center mt-4 font-montserrat text-2xl font-bold tracking-wider">
+        CSE From Home -{" "}
+        <span className="italic font-extrabold bg-gradient-to-r from-purple-800 via-accent to-purple-400 inline-block text-transparent bg-clip-text">
+          Kotlin
+        </span>
+      </h1>
+      <div className="flex mt-10 gap-x-10">
         <div>
           <iframe
-            width="560"
-            height="315"
+            width="800"
+            height="450"
             src="https://www.youtube.com/embed/F9UC9DY-vIU?si=2V3IKmgh_ZbkiW1Q"
             title="YouTube video player"
             frameborder="0"
@@ -15,13 +21,29 @@ const MyClass = ({ course }) => {
             allowfullscreen
           ></iframe>
         </div>
-        <ul>
-          <li>Class 1</li>
-          <li>Class 2</li>
-          <li>Class 3</li>
-          <li>Class 1</li>
-          <li>Class 4</li>
-        </ul>
+        <div className="transition-all duration-1000 bg-accent w-full list-none p-2">
+          <details className="border border-gray-600 p-2">
+            <summary className="cursor-pointer">Kotlin</summary>
+            <div className="pl-4">
+              <details>
+                <summary className="cursor-pointer">
+                  Module1: Understanding of kotlin
+                </summary>
+                <div className="pl-4">
+                  <summary className="cursor-pointer">
+                    Video1: Let's start with kotlin
+                  </summary>
+                  <summary className="cursor-pointer">
+                    Video2: Fundamental of kotlin
+                  </summary>
+                  <summary className="cursor-pointer">
+                    Video2: Fundamental of kotlin
+                  </summary>
+                </div>
+              </details>
+            </div>
+          </details>
+        </div>
       </div>
     </div>
   );
