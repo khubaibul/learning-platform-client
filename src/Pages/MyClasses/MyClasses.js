50ee3fac57057e4b601cf4823b0d9055d6ae5075
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-11-14 21:58:36 +0600

    Course Div Conmplete

--- src/Pages/MyClasses/MyClasses.js
+++ src/Pages/MyClasses/MyClasses.js
@@ -19,13 +19,29 @@ const MyClasses = () => {
         {myClasses?.map((myClass) => (
           <>
             <div className="bg-gradient-to-l from-[#1B0B30] to-[#100530] p-8 rounded-lg flex gap-x-8">
-              <img className="h-56 w-96" src={kotlin} alt="" />
-              <div>
+              <img className="h-56 w-96 rounded-md" src={kotlin} alt="" />
+              <div className="flex flex-col gap-y-8 justify-center">
                 <h1 className="bg-gradient-to-r from-purple-800 via-accent to-purple-400 inline-block text-transparent bg-clip-text text-2xl font-montserrat font-bold">
                   Complete {myClass?.courseName} Course By CSE FROM HOME
                 </h1>
-                <div class="p-0.5 text-center text-xs font-medium leading-none text-primary-100 w-full bg-gradient-to-r from-purple-800 via-accent to-purple-400 rounded-full text-white">
+                <div class="p-1 text-center text-sm font-montserrat font-medium leading-none text-primary-100 w-full bg-gradient-to-r from-purple-800 via-accent to-purple-400 rounded-full text-white">
                   100%
+                </div>
+                <div className="flex gap-x-5 text-lg font-montserrat">
+                  <button
+                    // onClick={() => nextContent()}
+                    className="bg-gradient-to-r from-purple-600 hover:from-purple-700 to-violet-500 hover:to-violet-600 px-4 py-0.5 rounded-md font-semibold text-slate-900"
+                  >
+                    <span className="bg-gradient-to-r from-white to-sky-400 inline-block text-transparent bg-clip-text font-montserrat">
+                      Continue Course
+                    </span>
+                  </button>
+                  <button
+                    // onClick={() => nextContent()}
+                    className="module-border-wrap px-4 py-0.5 rounded-md text-violet-800 font-semibold"
+                  >
+                    Outline
+                  </button>
                 </div>
               </div>
             </div>
