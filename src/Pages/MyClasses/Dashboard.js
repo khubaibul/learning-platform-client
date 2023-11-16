8945ab438c8d90f008f37e38ec2e769bf6484ca4
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-11-16 19:46:00 +0600

    Quote Added

--- src/Pages/MyClasses/Dashboard.js
+++ src/Pages/MyClasses/Dashboard.js
@@ -22,16 +22,18 @@ const Dashboard = () => {
 
       <div className="grid grid-cols-5 gap-x-5 mb-10">
         <div className="col-span-4 bg-gradient-to-r from-purple-800 via-accent to-purple-400 p-[3px] rounded-lg">
-          <div className="bg-gradient-to-l from-[#1B0B30] to-[#1e0f4b] p-8 rounded-lg h-full">
+          <div className="bg-gradient-to-l from-[#1B0B30] to-[#1e0f4b] p-8 rounded-lg h-full flex gap-x-8">
             <img
               className="h-56 w-96 rounded-md"
               src={courseBrandImage}
               alt=""
             />
             <div className="flex flex-col gap-y-8 justify-center">
-              <h1 className="bg-gradient-to-r from-purple-800 via-accent to-purple-400 inline-block text-transparent bg-clip-text text-2xl font-montserrat font-bold">
-                Complete Course By CSE FROM HOME
-              </h1>
+              <div>
+                <h1 className="bg-gradient-to-r from-purple-800 via-accent to-purple-400 inline-block text-transparent bg-clip-text italic text-xl font-montserrat font-bold">
+                  "Unlock the future, writing one line of code."
+                </h1>
+              </div>
               {/* <div class="p-1 text-center text-sm font-montserrat font-medium leading-none text-primary-100 w-full bg-gradient-to-r from-purple-800 via-accent to-purple-400 rounded-full text-white">
                 100%
               </div> */}
@@ -42,14 +44,8 @@ const Dashboard = () => {
                   className="flex justify-center items-center bg-gradient-to-r from-purple-600 hover:from-purple-700 to-violet-500 hover:to-violet-600 px-4 py-0.5 rounded-md font-semibold text-slate-900"
                 >
                   <span className="bg-gradient-to-r from-white to-sky-400 inline-block text-transparent bg-clip-text font-montserrat">
-                    Continue Course
+                    Course Instructions
                   </span>
-                </button>
-                <button
-                  // onClick={() => nextContent()}
-                  className="module-border-wrap px-4 py-0.5 rounded-md text-violet-800 font-semibold"
-                >
-                  Outline
                 </button>
               </div>
             </div>
