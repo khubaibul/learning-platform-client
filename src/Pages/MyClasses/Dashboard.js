848997f1798434ebcd49ce03e5b4ca8a6b4afdf5
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-11-16 19:58:25 +0600

    Dashboard Complete

--- src/Pages/MyClasses/Dashboard.js
+++ src/Pages/MyClasses/Dashboard.js
@@ -23,13 +23,13 @@ const Dashboard = () => {
 
       <div className="grid grid-cols-5 gap-x-5 mb-10">
         <div className="col-span-4 bg-gradient-to-r from-purple-800 via-accent to-purple-400 p-[3px] rounded-lg">
-          <div className="bg-gradient-to-l from-[#1B0B30] to-[#1e0f4b] p-8 rounded-lg h-full flex gap-x-8">
+          <div className="bg-gradient-to-l from-[#1B0B30] to-[#1e0f4b] p-8 rounded-lg h-full flex items-center gap-x-8">
             <img
               className="h-56 w-96 rounded-md"
               src={courseBrandImage}
               alt=""
             />
-            <div className="flex flex-col gap-y-8 justify-center">
+            <div className="flex flex-col gap-y-4 justify-center">
               <div>
                 <h1 className="bg-gradient-to-r from-purple-800 via-accent to-purple-400 inline-block text-transparent bg-clip-text italic text-xl font-montserrat font-bold">
                   "Unlock the future, writing one line of code."
@@ -38,6 +38,16 @@ const Dashboard = () => {
                   <img className="w-5 h-5" src={quote} alt="" />
                   <span className="text-purple-800">Khubaibul Islam</span>
                 </div>
+              </div>
+              <div>
+                <h3 className="text-lg font-montserrat text-purple-700 font-semibold text-justify">
+                  From today you have only one mission - to become a next level
+                  <span className="bg-gradient-to-r from-purple-400 via-accent to-purple-800 inline-block text-transparent bg-clip-text italic font-bold mx-1.5">
+                    Computer Science Engineer.
+                  </span>
+                  You will work hard in this mission. And we will support as
+                  much as possible.
+                </h3>
               </div>
               <div className="flex gap-x-5 text-lg font-montserrat">
                 <button className="flex justify-center items-center bg-gradient-to-r from-purple-600 hover:from-purple-700 to-violet-500 hover:to-violet-600 px-4 py-0.5 rounded-md font-semibold text-slate-900">
