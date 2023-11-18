5d2581b7f959fcdf47a7e03c580a7dfa0cef868b
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-11-19 00:36:54 +0600

    Hero Card Section Added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { useContext } from "react";
 import { Link, useLoaderData } from "react-router-dom";
-import heroBg from "../../Assets/hero-bg.jpg";
+import dataScience from "../../Assets/data science.png";
+import machineLearning from "../../Assets/machine learning.png";
+import programming from "../../Assets/programming.png";
+import ai from "../../Assets/ai.png";
+import network from "../../Assets/network.png";
+import ethicalHacking from "../../Assets/ethical hacking.png";
+import cloudComputing from "../../Assets/cloud computing.png";
 import bgVideo from "../../Assets/Coding Hack Video.mp4";
 import { AuthContext } from "../../Context/AuthProvider/AuthProvider";
 import HomeCourseCard from "../HomeCourseCard/HomeCourseCard";
@@ -11,18 +17,56 @@ import ContactUs from "./ContactUs";
 
 const Home = () => {
   const { data } = useLoaderData();
-
   const { theme } = useContext(AuthContext);
+
+  const skillCards = [
+    {
+      name: "Data Science",
+      image: dataScience,
+    },
+    {
+      name: "Machine Learning",
+      image: machineLearning,
+    },
+    {
+      name: "Programming & Development",
+      image: programming,
+    },
+    {
+      name: "Artificial Intelligence",
+      image: ai,
+    },
+    {
+      name: "Network System",
+      image: network,
+    },
+    {
+      name: "Ethical Hacking",
+      image: ethicalHacking,
+    },
+    {
+      name: "Cloud Computing",
+      image: cloudComputing,
+    },
+  ];
 
   return (
     <div className="mb-28">
       <div className="hero-main-section w-[100%]">
         <video className="bg-video" src={bgVideo} autoPlay muted loop />
         <div className="hero-content">
-          <div>
+          <div className="mt-10">
             <h1 className="font-publicSans text-7xl text-white text-center">
               Learn new skills. Prove your potential.
             </h1>
+            <div className="flex justify-center gap-3">
+              {skillCards?.map((skillCard) => (
+                <div className="bg-slate-300 p-4 w-36 h-48 flex flex-col justify-center items-center text-center rounded">
+                  <img src={skillCard?.image} className="w-16" alt="" />
+                  <h3>{skillCard?.name}</h3>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
