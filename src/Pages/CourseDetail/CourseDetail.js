91ab9cf68ee60e56523cfbb9e66288350c74b1cb
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-11-27 23:03:30 +0600

    done

--- src/Pages/CourseDetail/CourseDetail.js
+++ src/Pages/CourseDetail/CourseDetail.js
@@ -4,6 +4,7 @@ import { faFilePdf, faTools } from "@fortawesome/free-solid-svg-icons";
 import { Link, useLoaderData } from "react-router-dom";
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import PDF from "../PDF/PDF";
+import bg from "../../Assets/website-bg.png";
 
 const CourseDetail = () => {
   const { data: courseDetail } = useLoaderData();
@@ -20,74 +21,84 @@ const CourseDetail = () => {
   } = courseDetail;
 
   return (
-    <div className="card lg:w-[40%] my-10 mx-auto bg-base-100 shadow-xl image-full rounded-none">
-      <figure>
-        <img src={courseImage} alt="Course Card Img" />
-      </figure>
-      <div className="card-body rounded-none">
-        <div className="flex justify-between mb-5">
-          <h2 className="card-title text-4xl font-montserrat">{courseTitle}</h2>
-          <PDFDownloadLink
-            document={<PDF courseDetail={courseDetail} />}
-            filename="FORM"
-          >
-            {({ loading }) =>
-              loading ? (
-                <button>Loading Document...</button>
-              ) : (
-                <button>
-                  <FontAwesomeIcon
-                    className="text-3xl"
-                    icon={faFilePdf}
-                  ></FontAwesomeIcon>
-                </button>
-              )
-            }
-          </PDFDownloadLink>
-        </div>
-        <p className="text-justify">
-          <span className="font-bold text-lg font-montserrat">
-            Course Description:{" "}
-          </span>{" "}
-          {course_description}
-        </p>
-        <p>
-          <span className="font-bold text-lg">Common Uses: </span> {common_uses}
-        </p>
-        <div>
-          <h4 className="text-xl font-bold my-2">Will Be Covered</h4>
-          <div className="grid grid-cols-2">
-            {course_module_topics?.map((topic, idx) => {
-              return (
-                <div key={idx}>
-                  <li className="list-none">
-                    <FontAwesomeIcon icon={faTools}></FontAwesomeIcon> {topic}
-                  </li>
-                </div>
-              );
-            })}
+    <div 
+    style={{ backgroundImage: `url(${bg})` }} 
+    className=""
+    >
+      <div className="card lg:w-[40%] my-10 mx-auto bg-base-100 shadow-xl image-full rounded-none">
+        <figure>
+          <img src={courseImage} alt="Course Card Img" />
+        </figure>
+        <div className="card-body rounded-none">
+          <div className="flex justify-between mb-5">
+            <h2 className="card-title text-4xl font-montserrat">
+              {courseTitle}
+            </h2>
+            <PDFDownloadLink
+              document={<PDF courseDetail={courseDetail} />}
+              filename="FORM"
+            >
+              {({ loading }) =>
+                loading ? (
+                  <button>Loading Document...</button>
+                ) : (
+                  <button>
+                    <FontAwesomeIcon
+                      className="text-3xl"
+                      icon={faFilePdf}
+                    ></FontAwesomeIcon>
+                  </button>
+                )
+              }
+            </PDFDownloadLink>
           </div>
-        </div>
-        <div className="flex gap-x-2 my-3">
-          <div className="flex gap-x-2 text-sm bg-accent/50 px-3 py-0.5 font-semibold">
-            <p>PRICE:</p>
-            <p>BDT{course_price}</p>
+          <p className="text-justify">
+            <span className="font-bold text-lg font-montserrat">
+              Course Description:{" "}
+            </span>{" "}
+            {course_description}
+          </p>
+          <p>
+            <span className="font-bold text-lg">Common Uses: </span>{" "}
+            {common_uses}
+          </p>
+          <div>
+            <h4 className="text-xl font-bold my-2">Will Be Covered</h4>
+            <div className="grid grid-cols-2">
+              {course_module_topics?.map((topic, idx) => {
+                return (
+                  <div key={idx}>
+                    <li className="list-none">
+                      <FontAwesomeIcon icon={faTools}></FontAwesomeIcon> {topic}
+                    </li>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="flex gap-x-2 text-sm bg-accent/50 px-3 py-0.5 font-semibold">
-            <p>DURATION:</p>
-            <p>{course_duration}</p>
+          <div className="flex gap-x-2 my-3">
+            <div className="flex gap-x-2 text-sm bg-accent/50 px-3 py-0.5 font-semibold">
+              <p>PRICE:</p>
+              <p>BDT{course_price}</p>
+            </div>
+            <div className="flex gap-x-2 text-sm bg-accent/50 px-3 py-0.5 font-semibold">
+              <p>DURATION:</p>
+              <p>{course_duration}</p>
+            </div>
+            <div className="flex gap-x-2 text-sm bg-accent/50 px-3 py-0.5 font-semibold">
+              <p>MODULE:</p>
+              <p>{course_module}</p>
+            </div>
           </div>
-          <div className="flex gap-x-2 text-sm bg-accent/50 px-3 py-0.5 font-semibold">
-            <p>MODULE:</p>
-            <p>{course_module}</p>
+          <div className="card-actions justify-center">
+            <button className="btn btn-outline btn-accent rounded-none font-montserrat px-10">
+              <Link
+                to={`/course/checkout/${_id}/${course_price}/${courseTitle}`}
+              >
+                Get Premium Access
+              </Link>
+            </button>
           </div>
-        </div>
-        <div className="card-actions justify-center">
-          <button className="btn btn-outline btn-accent rounded-none font-montserrat px-10">
-            <Link to={`/course/checkout/${_id}/${course_price}/${courseTitle}`}>
-              Get Premium Access
-            </Link>
-          </button>
         </div>
       </div>
     </div>
