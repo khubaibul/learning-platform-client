d9ecb537ea89aa537c795c209f76f0e762b29b92
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-11-29 23:44:40 +0600

    Userprofile updating

--- src/Pages/User/UserProfile/UserProfile.js
+++ src/Pages/User/UserProfile/UserProfile.js
@@ -19,85 +19,93 @@ const UserProfile = () => {
   return (
     <div>
       {user ? (
-        <div
-          className={` ${
-            theme ? "bg-gray-900" : "bg-yellow-400"
-          } font-sans h-screen w-full flex flex-row justify-center items-center`}
-        >
-          <div
-            className={`card w-96 mx-auto ${
-              theme ? "bg-slate-700 text-slate-300" : "bg-white"
-            } rounded-none`}
-          >
-            <img
-              className="w-32 mx-auto rounded-full border-8 border-white"
-              src={user.photoURL}
-              alt=""
-            />
-            <div className="text-center mt-2 text-2xl font-medium font-publicSans">
-              {user?.displayName}
-            </div>
-            {user.email && (
-              <div className="text-center mt-2 font-light text-sm flex justify-center items-center gap-x-2">
-                <FontAwesomeIcon icon={faG}></FontAwesomeIcon>
-                {user?.email}
-              </div>
-            )}
-            <small className="text-center font-semibold text-base">
-              <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>{" "}
-              {user?.uid.toUpperCase()}
-            </small>
-            <hr className="mt-8" />
-            <h2 className="text-lg font-semibold font-montserrat text-center">
-              My Courses
-            </h2>
-            {myCourses.length > 0 ? (
-              <div className="flex flex-col gap-2 p-4">
-                {myCourses?.map((myCourse) => (
-                  <div className="border border-yellow-400 p-2 flex justify-between items-center">
-                    <h3 className="font-montserrat font-semibold">
-                      {myCourse?.courseName}
-                    </h3>
-                    <h3 className="text-xs">{myCourse?.transactionId}</h3>
-                    {myCourse?.paid === true ? (
-                      <Link
-                        to={`/myClass/${myCourse?.courseName}`}
-                        className={`font-semibold rounded-sm p-0.5 text-center hover:bg-opacity-80 active:bg-opacity-100 w-[30%] bg-green-500`}
-                      >
-                        Go To Class
-                      </Link>
-                    ) : (
-                      <Link
-                        to="/"
-                        className={`font-semibold rounded-sm p-0.5 text-center hover:bg-opacity-80 active:bg-opacity-100 w-[30%] bg-red-300`}
-                      >
-                        Pay Now
-                      </Link>
-                    )}
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <h4
-                className={
-                  theme ? "text-center py-5 text-[#EAAAFF]" : "text-center py-5"
-                }
-              >
-                You haven't enrolled any course yet. <br /> Please enroll our{" "}
-                <Link
-                  to="/courses"
-                  className="text-blue-500 font-semibold italic"
-                >
-                  courses
-                </Link>
-              </h4>
-            )}
-            <button
-              onClick={logOut}
-              className="btn btn-sm btn-outline btn-accent rounded-none w-[30%] mx-auto mb-4 font-montserrat"
-            >
-              Log Out
-            </button>
+        // <div
+        //   className={` ${
+        //     theme ? "bg-gray-900" : "bg-yellow-400"
+        //   } font-sans h-screen w-full flex flex-row justify-center items-center`}
+        // >
+        //   <div
+        //     className={`card w-96 mx-auto ${
+        //       theme ? "bg-slate-700 text-slate-300" : "bg-white"
+        //     } rounded-none`}
+        //   >
+        //     <img
+        //       className="w-32 mx-auto rounded-full border-8 border-white"
+        //       src={user.photoURL}
+        //       alt=""
+        //     />
+        //     <div className="text-center mt-2 text-2xl font-medium font-publicSans">
+        //       {user?.displayName}
+        //     </div>
+        //     {user.email && (
+        //       <div className="text-center mt-2 font-light text-sm flex justify-center items-center gap-x-2">
+        //         <FontAwesomeIcon icon={faG}></FontAwesomeIcon>
+        //         {user?.email}
+        //       </div>
+        //     )}
+        //     <small className="text-center font-semibold text-base">
+        //       <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>{" "}
+        //       {user?.uid.toUpperCase()}
+        //     </small>
+        //     <hr className="mt-8" />
+        //     <h2 className="text-lg font-semibold font-montserrat text-center">
+        //       My Courses
+        //     </h2>
+        //     {myCourses.length > 0 ? (
+        //       <div className="flex flex-col gap-2 p-4">
+        //         {myCourses?.map((myCourse) => (
+        //           <div className="border border-yellow-400 p-2 flex justify-between items-center">
+        //             <h3 className="font-montserrat font-semibold">
+        //               {myCourse?.courseName}
+        //             </h3>
+        //             <h3 className="text-xs">{myCourse?.transactionId}</h3>
+        //             {myCourse?.paid === true ? (
+        //               <Link
+        //                 to={`/myClass/${myCourse?.courseName}`}
+        //                 className={`font-semibold rounded-sm p-0.5 text-center hover:bg-opacity-80 active:bg-opacity-100 w-[30%] bg-green-500`}
+        //               >
+        //                 Go To Class
+        //               </Link>
+        //             ) : (
+        //               <Link
+        //                 to="/"
+        //                 className={`font-semibold rounded-sm p-0.5 text-center hover:bg-opacity-80 active:bg-opacity-100 w-[30%] bg-red-300`}
+        //               >
+        //                 Pay Now
+        //               </Link>
+        //             )}
+        //           </div>
+        //         ))}
+        //       </div>
+        //     ) : (
+        //       <h4
+        //         className={
+        //           theme ? "text-center py-5 text-[#EAAAFF]" : "text-center py-5"
+        //         }
+        //       >
+        //         You haven't enrolled any course yet. <br /> Please enroll our{" "}
+        //         <Link
+        //           to="/courses"
+        //           className="text-blue-500 font-semibold italic"
+        //         >
+        //           courses
+        //         </Link>
+        //       </h4>
+        //     )}
+        //     <button
+        //       onClick={logOut}
+        //       className="btn btn-sm btn-outline btn-accent rounded-none w-[30%] mx-auto mb-4 font-montserrat"
+        //     >
+        //       Log Out
+        //     </button>
+        //   </div>
+        // </div>
+        <div>
+          <div>
+            <p>Sidebar</p>
+          </div>
+          <div>
+            <h1>Section</h1>
           </div>
         </div>
       ) : (
