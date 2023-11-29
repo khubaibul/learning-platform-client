704509bd4b781f65881c8571b0064d4a96b16095
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-11-29 20:43:35 +0600

    Button Added

--- src/Pages/Home/LandingPage.js
+++ src/Pages/Home/LandingPage.js
@@ -56,7 +56,7 @@ const LandingPage = () => {
                           follow.
                         </p>
                       </div>
-                      <button className="font-montserrat btn btn-sm btn-accent btn-outline rounded-none">
+                      <button className="hover:bg-gradient-to-l from-[#A12350] via-[#60277B] to-[#362298] border-2 border-accent font-medium  font-publicSans tracking-widest py-2 px-8 text-[#f25189] rounded transition-all duration-300">
                         Browse Our Paths
                       </button>
                     </div>
