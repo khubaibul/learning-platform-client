b9ca3d93a9d9981f064b94df6a0d7d91c0afa089
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-11-29 23:50:43 +0600

    Div Done

--- src/Pages/User/UserProfile/UserProfile.js
+++ src/Pages/User/UserProfile/UserProfile.js
@@ -100,11 +100,11 @@ const UserProfile = () => {
         //     </button>
         //   </div>
         // </div>
-        <div className="grid grid-cols-5 gap-x-10 lg:w-[90%] mx-auto">
-          <div className="bg-[#0D0D21] col-span-1">
+        <div className="grid grid-cols-7 gap-x-10 lg:w-[85%] mx-auto h-screen">
+          <div className="bg-[#0D0D21] col-span-2 rounded-md">
             <p>Sidebar</p>
           </div>
-          <div className="bg-[#0D0D21] col-span-4">
+          <div className="bg-[#0D0D21] col-span-5">
             <h1>Section</h1>
           </div>
         </div>
