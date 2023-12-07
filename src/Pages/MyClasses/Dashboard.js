c0f9889a23a6f1ce6eb8a499c3e0705b0a86b2ee
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-12-07 23:16:58 +0600

    D

--- src/Pages/MyClasses/Dashboard.js
+++ src/Pages/MyClasses/Dashboard.js
@@ -9,7 +9,9 @@ const Dashboard = () => {
   return (
     <div className="w-[80%] mx-auto mb-28">
       <h1
-        className={`text-center mt-16 mb-10 font-montserrat text-2xl font-bold tracking-wider text-[#EAAAFF]`}
+        className={`text-center mt-16 mb-10 font-montserrat text-2xl font-bold tracking-wider ${
+          theme ? "text-gray-800" : "text-[#EAAAFF]"
+        }`}
       >
         Welcome Back{" "}
         <span className="italic font-extrabold bg-gradient-to-r from-purple-800 via-accent to-purple-400 inline-block text-transparent bg-clip-text">
