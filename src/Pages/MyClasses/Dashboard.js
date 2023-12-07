81d185d1d37f212774d3402f3f19506393ffe148
Khubaibul Islam <khubaibulislam2001@gmail.com>
2023-12-07 23:23:47 +0600

    R

--- src/Pages/MyClasses/Dashboard.js
+++ src/Pages/MyClasses/Dashboard.js
@@ -20,7 +20,7 @@ const Dashboard = () => {
         , Ready For Your Next Lesson?
       </h1>
 
-      <div className="grid grid-cols-5 gap-x-5">
+      <div className="lg:grid md:grid grid-cols-5 flex flex-col gap-5">
         <div className="col-span-4 bg-gradient-to-r from-purple-400 via-accent to-purple-800 p-[3px] rounded-lg">
           <div className="bg-gradient-to-l from-[#1B0B30] to-[#1e0f4b] p-8 rounded-lg h-full flex items-center gap-x-8">
             <img
